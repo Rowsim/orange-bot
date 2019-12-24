52cbfd517de7d40f1664a31e2f1a4c4ace74d5e9
Rowsim <rowanq@live.co.uk>
2019-12-24 20:48:18 +0000

    Added ping every 5mins to prevent glitch server from sleeping

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require('dotenv').config()
 const Schedule = require('node-schedule');
 const Discord = require('discord.js')
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 const client = new Discord.Client()
 
