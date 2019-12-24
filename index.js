e7905af350e4fe8f2ed87ecf33e5d91827b71ed6
Rowsim <rowanq@live.co.uk>
2019-12-24 17:07:31 +0000

    !r teamcomps message in cron job

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const Schedule = require('node-schedule');
 const Discord = require('discord.js')
 
 const client = new Discord.Client()
@@ -7,12 +8,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
-    console.log(msg)
-    if (msg.channel.id === '659065267095076864') //TFT Text channel
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
-})
+// client.on('message', msg => {
+//     console.log(msg)
+//     if (msg.channel.id === '659065267095076864') //TFT Text channel
+//   if (msg.content === 'ping') {
+//     msg.reply('Pong!')
+//   }
+// })
 
 client.login(process.env.BOT_TOKEN)
+
+Schedule.scheduleJob('53 * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
+    const currentDate = new Date();
+    client.channels.get('659065267095076864')
+    .send(`!r teamcomps\n>TFT Team Comps ${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`)
+  });
